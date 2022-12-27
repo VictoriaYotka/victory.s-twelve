c12152e4586124bc191ef9dde21b43738a8702fb
Heorhii <gerythepatient@gmail.com>
2022-12-27 16:40:11 +0100

    Edit index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-(() => {
-  const e = {
-    openProductBtn: document.querySelector('[data-iceCream-open]'),
-    closeProductBtn: document.querySelector('[data-iceCream-close]'),
-    menu: document.querySelector('[data-iceCream]'),
-  };
-  function n() {
-    e.menu.classList.toggle('is-hidden');
-  }
-  e.openProductBtn.addEventListener('click', n),
-    e.closeProductBtn.addEventListener('click', n);
-})(),
-  (() => {
-    const e = {
-      openProductBtn: document.querySelector('[data-iceCoffee-open]'),
-      closeProductBtn: document.querySelector('[data-iceCoffee-close]'),
-      menu: document.querySelector('[data-iceCoffee]'),
-    };
-    function n() {
-      e.menu.classList.toggle('is-hidden');
-    }
-    e.openProductBtn.addEventListener('click', n),
-      e.closeProductBtn.addEventListener('click', n);
-  })(),
-  (() => {
-    const e = {
-      openProductBtn: document.querySelector('[data-milkshakes-open]'),
-      closeProductBtn: document.querySelector('[data-milkshakes-close]'),
-      menu: document.querySelector('[data-milkshakes]'),
-    };
-    function n() {
-      e.menu.classList.toggle('is-hidden');
-    }
-    e.openProductBtn.addEventListener('click', n),
-      e.closeProductBtn.addEventListener('click', n);
-  })();
+import './js/gallery-slider';
+import './js/anchor-scroll';
+import './js/mobile-menu';
+import './js/slick';
+import './js/text_appear';
+import './js/modal-buynow';
+import './js/modal-franchise';
+import './js/modal-ingredients';
+import './js/modal-location';
+import './js/modal-readmore';
