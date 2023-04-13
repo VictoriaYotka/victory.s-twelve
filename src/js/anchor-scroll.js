70bafb67c682eee09863735f2c9412c621ccfd95
Mikhailo Salko <slipknotmisha@gmail.com>
2023-04-13 22:10:11 +0300

    delete old code, add smooth anchor scroll for hero buttons

--- src/js/anchor-scroll.js
+++ src/js/anchor-scroll.js
@@ -13,27 +13,15 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function () {
-//   // Add smooth scrolling to all links
-//   $('a').on('click', function (event) {
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== '') {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
-
-//       // Store hash
-//       let hash = this.hash;
-
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $('html, body').animate(
-//         { scrollTop: $(hash).offset().top - 10 },
-//         800,
-//         function () {
-//           // Add hash (#) to URL when done scrolling (default click behavior)
-//           window.location.hash = hash;
-//         }
-//       );
-//     }
-//   });
-// });
+$(document).ready(function () {
+  $('#hero-buttons').on('click', 'a', function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
